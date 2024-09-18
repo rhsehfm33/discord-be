e01d66ea1b618d0d6aa7c3262a949ce7472765fb
임민성 <rhsehfm33@gmail.com>
2024-09-18 21:16:49 +0900

    test Make user transaction test

--- src/test/java/team/discordbe/test/integration/UserIntegrationTest.java
+++ src/test/java/team/discordbe/test/integration/UserIntegrationTest.java
@@ -1,11 +1,20 @@
 package team.discordbe.test.integration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 
 import team.discordbe.domain.user.dto.UserRequestDto;
+import team.discordbe.domain.user.dto.UserResponseDto;
 import team.discordbe.domain.user.services.UserService;
 import team.discordbe.test.BaseIntegrationTest;
 import team.discordbe.util.InstanceSetter;
@@ -24,6 +33,35 @@ public class UserIntegrationTest extends BaseIntegrationTest {
         userService.createdUser(userRequestDto);
         Assertions.assertThrows(DuplicateKeyException.class, () -> {
             userService.createdUser(userRequestDto);
+        });
+    }
+
+    // 사용자를 동시에 생성했을 때, 중복된 필드가 발생하면 에러가 발생해야 함
+    @Test
+    public void testConcurrentUserDuplication()
+        throws NoSuchFieldException, IllegalAccessException {
+        int numberOfThreads = 3;
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        List<Callable<UserResponseDto>> tasks = new ArrayList<>();
+        for (int i = 0; i < numberOfThreads; ++i) {
+            UserRequestDto userRequestDto = new UserRequestDto();
+            InstanceSetter.setField(userRequestDto, "email", "test_user@gmail.com");
+            InstanceSetter.setField(userRequestDto, "nickName", "test_user");
+            InstanceSetter.setField(userRequestDto, "password", "asdqwe12#");
+            tasks.add(() -> userService.createdUser(userRequestDto));
+        }
+
+        Assertions.assertThrows(ExecutionException.class, () -> {
+            List<Future<UserResponseDto>> results = executorService.invokeAll(tasks);
+
+            for (Future<UserResponseDto> result : results) {
+                try {
+                    result.get();
+                } catch (ExecutionException e) {
+                    throw e;
+                }
+            }
         });
     }
 }
