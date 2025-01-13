46f05187e225bd2e9d2487e43ae42567d806ebc5
임민성 <rhsehfm33@gmail.com>
2025-01-13 22:45:39 +0900

    feat Implement basic websocket in chat-gateway

---
+++ chat-gateway/src/main/java/discord/chat/gateway/interfaces/common/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package discord.chat.gateway.interfaces.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")  // WebSocket 연결 endpoint
+                .setAllowedOrigins("http://www.msdiscord.site:5173")  // CORS 허용 설정
+                .withSockJS();
+    }
+}
